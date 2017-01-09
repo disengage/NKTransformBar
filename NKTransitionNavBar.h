017cb93629bca7ba927eece334adff550dd51d00
Narongsak kongpan <disengage.nrskp@gmail.com>
2017-01-09 22:45:00 +0700

    Add images

--- NKTransitionNavBar.h
+++ NKTransitionNavBar.h
@@ -1,6 +1,6 @@
 //
 //  NKTransitionNavBar.h
-//  TransitionNavBar-Test
+//  Narongsak kongpan
 //
 //  Created by Narongsak kongpan on 6/14/2558 BE.
 //  Copyright (c) 2558 Narongsak kongpan. All rights reserved.
@@ -9,15 +9,25 @@
 #import <UIKit/UIKit.h>
 
 @protocol NKTransitionNavBar_Protocol <NSObject>
+@optional
 -(void)onTransitionNavBarShow:(BOOL)showing;
 @end
 
+typedef enum {
+    NKTransitionNavBarToggleView,
+    NKTransitionNavBarPullView
+} NKTransitionNavBarStyle;
+
 @interface NKTransitionNavBar : UIView <UIGestureRecognizerDelegate>
 -(instancetype)initWithRootViewController:(UIViewController*)rootViewController andBarButton:(UIBarButtonItem*)targetButton;
+-(void)setContentViewController:(UIViewController*)view;
+-(void)hide;
+-(void)show;
 
 @property (nonatomic) BOOL isShowing;
 @property (nonatomic) CGFloat sideBarWidth;
 @property (nonatomic) float sideBarDeley;
+@property (nonatomic) NKTransitionNavBarStyle transitionStyle;
 @property (nonatomic, retain) id<NKTransitionNavBar_Protocol> delegate;
 
 @end
--- NKTransitionNavBar.m
+++ NKTransitionNavBar.m
@@ -1,6 +1,6 @@
 //
 //  NKTransitionNavBar.m
-//  TransitionNavBar-Test
+//  Narongsak kongpan
 //
 //  Created by Narongsak kongpan on 6/14/2558 BE.
 //  Copyright (c) 2558 Narongsak kongpan. All rights reserved.
@@ -19,30 +19,45 @@
 
 	UISwipeGestureRecognizer *swipeGestureToLeft;
 	UISwipeGestureRecognizer *swipeGestureToRight;
+    UITapGestureRecognizer *singleTapGesture;
+    
+    UIViewController *_contentController;
 }
 
 @synthesize isShowing = _isShowing;
 @synthesize delegate = _delegate;
 @synthesize sideBarWidth = _sideBarWidth;
 @synthesize sideBarDeley = _sideBarDeley;
+@synthesize transitionStyle = _transitionStyle;
 
 -(instancetype)initWithRootViewController:(UIViewController*)rootViewController andBarButton:(UIBarButtonItem*)targetButton{
-	self = [super initWithFrame:rootViewController.view.frame];
+	//self = [super initWithFrame:rootViewController.view.frame];
+    self = [super initWithFrame:[UIScreen mainScreen].bounds];
 	if (self) {
 		_rootViewController = rootViewController;
 		_barButton = targetButton;
-		rootViewRect = rootViewController.view.frame;
+        _transitionStyle = NKTransitionNavBarToggleView;
+        //rootViewRect = rootViewController.view.frame;
+        rootViewRect = [UIScreen mainScreen].bounds;
 
-		swipeGestureToLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(onSwipeGestureRecognizer:)];
+		swipeGestureToLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self
+                                                                       action:@selector(onSwipeGestureRecognizer:)];
 		[swipeGestureToLeft setDelegate:self];
 		[swipeGestureToLeft setNumberOfTouchesRequired:1];
 		[swipeGestureToLeft setDirection:UISwipeGestureRecognizerDirectionLeft];
 
-		swipeGestureToRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(onSwipeGestureRecognizer:)];
+		swipeGestureToRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self
+                                                                        action:@selector(onSwipeGestureRecognizer:)];
 		[swipeGestureToRight setDelegate:self];
 		[swipeGestureToRight setNumberOfTouchesRequired:1];
 		[swipeGestureToRight setDirection:UISwipeGestureRecognizerDirectionRight];
-
+        
+        singleTapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                   action:@selector(onSingleTapGestureRecognizer:)];
+        [singleTapGesture setDelegate:self];
+        [singleTapGesture setNumberOfTapsRequired:1];
+        [singleTapGesture setNumberOfTouchesRequired:1];
+        
 		[targetButton setTarget:self];
 		[targetButton setAction:@selector(onClickBtnTarget:)];
 		[rootViewController.view addSubview:self];
@@ -50,13 +65,23 @@
 		[self setBackgroundColor:[UIColor clearColor]];
 		[self addGestureRecognizer:swipeGestureToLeft];
 		[self addGestureRecognizer:swipeGestureToRight];
+        [self addGestureRecognizer:singleTapGesture];
 		self.sideBarDeley = 0.1;
 
 		[rootViewController.view addGestureRecognizer:swipeGestureToRight];
-
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(hide)
+                                                     name:@"applicationDidEnterBackground"
+                                                   object:nil];
 		[self wantToShow:NO];
 	}
 	return self;
+}
+
+-(void)onSingleTapGestureRecognizer:(UITapGestureRecognizer*)gestureRecognizer{
+    if (self.isShowing) {
+        [self wantToShow:NO];
+    }
 }
 
 -(void)onSwipeGestureRecognizer:(UISwipeGestureRecognizer*)gestureRecognizer{
@@ -70,21 +95,34 @@
 }
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch{
-	if ([[gestureRecognizer view] isKindOfClass:[self class]]) {
-		return YES;
-	}
-	if ([[gestureRecognizer view] isEqual:_rootViewController.view]) {
-		return YES;
-	}
+    BOOL innerRect = CGRectContainsPoint(sideBarView.frame, [touch locationInView:self]);
+    if([gestureRecognizer isKindOfClass:[UITapGestureRecognizer class]]){
+        if (self.isShowing) {
+            if (!innerRect) {
+                if ([[gestureRecognizer view] isKindOfClass:[self class]]) {
+                    return YES;
+                }
+            }
+        }
+    }else if([gestureRecognizer isKindOfClass:[UISwipeGestureRecognizer class]]){
+        if (self.isShowing) {
+            if (!innerRect) {
+                if ([[gestureRecognizer view] isKindOfClass:[self class]]) {
+                    return YES;
+                }
+            }
+        }else{
+            return YES;
+        }
+    }
 	return NO;
 }
 
 -(void)wantToShow:(BOOL)show{
 	if (show) {
 		if (self.sideBarWidth == 0) {
-			if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ||
-				UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomUnspecified) {
-				self.sideBarWidth = 100;
+			if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+				self.sideBarWidth = 260;
 			}else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
 				self.sideBarWidth = 320;
 			}
@@ -97,35 +135,67 @@
 			[sideBarView.layer setShadowOffset:CGSizeMake(0, 10)];
 			[sideBarView.layer setShadowOpacity:1];
 			[self addSubview:sideBarView];
+            
+            if (_contentController) {
+                [_contentController.view setFrame:CGRectMake(0, 0, self.sideBarWidth, rootViewRect.size.height)];
+                [sideBarView addSubview:_contentController.view];
+                [sideBarView setBackgroundColor:_contentController.view.backgroundColor];
+            }
 		}
-
-		[UIView animateWithDuration:0 animations:^{
-			sideBarView.transform = CGAffineTransformMakeTranslation(-sideBarView.frame.size.width, 0);
-		} completion:^(BOOL finished) {
-			[UIView animateWithDuration:self.sideBarDeley animations:^{
-				_rootViewController.view.transform = CGAffineTransformMakeTranslation(sideBarView.frame.size.width, 0);
-			} completion:^(BOOL finished) {
-				[self setUserInteractionEnabled:YES];
-
-				self.isShowing = YES;
-				[sideBarView.layer setShadowRadius:10];
-				[self showDelegate];
-			}];
-		}];
+        
+        switch (self.transitionStyle) {
+            case NKTransitionNavBarToggleView:{
+                [UIView animateWithDuration:self.sideBarDeley animations:^{
+                    sideBarView.transform = CGAffineTransformMakeTranslation(0, 0);
+                } completion:^(BOOL finished) {
+                    [self _isActive:YES];
+                }];
+            }
+                break;
+            case NKTransitionNavBarPullView:{
+                [UIView animateWithDuration:0 animations:^{
+                    sideBarView.transform = CGAffineTransformMakeTranslation(sideBarView.frame.size.width, 0);
+                } completion:^(BOOL finished) {
+                    [UIView animateWithDuration:self.sideBarDeley animations:^{
+                        _rootViewController.view.transform = CGAffineTransformMakeTranslation(sideBarView.frame.size.width, 0);
+                    } completion:^(BOOL finished) {
+                        [self _isActive:YES];
+                    }];
+                }];
+            }
+                break;
+        }
 	}else{
-		[UIView animateWithDuration:self.sideBarDeley
-						 animations:^{
-			_rootViewController.view.transform = CGAffineTransformMakeTranslation(0, 0);
-		} completion:^(BOOL finished) {
-			[self setUserInteractionEnabled:NO];
-
-			sideBarView.transform = CGAffineTransformMakeTranslation(-sideBarView.frame.size.width, 0);
-			self.isShowing = NO;
-			[sideBarView.layer setShadowRadius:0];
-
-			[self showDelegate];
-		}];
+        switch (self.transitionStyle) {
+            case NKTransitionNavBarToggleView:{
+                [UIView animateWithDuration:self.sideBarDeley animations:^{
+                    sideBarView.transform = CGAffineTransformMakeTranslation(-sideBarView.frame.size.width, 0);
+                } completion:^(BOOL finished) {
+                    [self _isActive:NO];
+                }];
+            }
+                break;
+            case NKTransitionNavBarPullView:{
+                [UIView animateWithDuration:self.sideBarDeley animations:^{
+                    _rootViewController.view.transform = CGAffineTransformMakeTranslation(0, 0);
+                } completion:^(BOOL finished) {
+                    [self _isActive:NO];
+                }];
+            }
+                break;
+        }
 	}
+}
+
+-(void)_isActive:(BOOL)active{
+    [self setUserInteractionEnabled:active];
+    self.isShowing = active;
+    
+    [sideBarView.layer setShadowRadius:(active) ? 10 : 0];
+    if (self.transitionStyle == NKTransitionNavBarPullView && !active) {
+        sideBarView.transform = CGAffineTransformMakeTranslation(-sideBarView.frame.size.width, 0);
+    }
+    [self showDelegate];
 }
 
 -(void)showDelegate{
@@ -138,6 +208,17 @@
 	[self wantToShow:YES];
 }
 
+-(void)show{
+    [self wantToShow:YES];
+}
+
+-(void)hide{
+    [self wantToShow:NO];
+}
+
+-(void)setContentViewController:(UIViewController *)view{
+    _contentController = view;
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
